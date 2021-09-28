faa8768302651e61206ae758c43a56814716ea25
Mirkoo03 <75308878+Mirkoo03@users.noreply.github.com>
2021-09-28 10:45:14 +0200

    fileModificati

---
+++ Client.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+String nomeServer ="LocalHost";
+
+int portaServer = 7000;
+
+Socket mySocket;
+
+BufferedReader inputTastiera;
+
+String stringaUtenteDigitata;
+
+String stringaRicevutaServer;
+
+DataOutputStream outPutVersoServer;
+
+BufferedReader inputDalServer;
+
+
+//metodo oer mettere in comunicazione il cliente col server
+
+public Socket connetti()
+{
+
+    System.out.println("Il client è partito per l'esecuzione");
+
+
+
+}
+
+    
+}
---
+++ Server_socket.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server_socket {
+
+
+    
+}
